0e64a8878c842b5c9eecfc01c3920398c4077860
MarcelusCH <buob.marcel@gmail.com>
2018-07-19 21:16:33 +0200

    Adding frontend files to Github pages

--- truffle.js
+++ truffle.js
@@ -19,14 +19,13 @@ module.exports = {
             host: "localhost",
             port: "8545",
             network_id: "4",
-            gas: 15000000,
-            gasPrice: 5000000000
+            gas: 4700000
           },
           live: {
             host: "localhost",
             port: "8545",
             network_id: "1",
-            gas: 100000000
+            gas: 4700000
           }
      }
 };
